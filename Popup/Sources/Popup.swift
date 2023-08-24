886d620f0fe0cd0c0168e3bab852c89b180e7191
galaxy <1035841713@qq.com>
2023-08-24 22:57:29 +0800

    Update | Modify RootView

--- Popup/Sources/Popup.swift
+++ Popup/Sources/Popup.swift
@@ -357,7 +357,7 @@ extension Popup {
     }
     
     private static func getRootView() -> UIView? {
-        // return Popup.getWindow()
-        return Popup.getRootViewController()?.view
+        return Popup.getWindow()
+        //return Popup.getRootViewController()?.view
     }
 }
